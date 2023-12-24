392a93b3186d461f684616a15944098f3caa6a6f
Muhammed-Ibrahem <muhammedibrahem001@gmail.com>
2023-12-24 14:34:52 +0200

    add: process section to the home-page

--- src/js/swipeEffect.js
+++ src/js/swipeEffect.js
@@ -1,4 +1,4 @@
-const valuesSwiper = new Swiper(".valuesSwiper", {
+const values_swiper = new Swiper(".valuesSwiper", {
   slidesPerView: 1,
   spaceBetween: 20,
   loop: true,
@@ -16,5 +16,19 @@ const valuesSwiper = new Swiper(".valuesSwiper", {
     1200: {
       slidesPerView: 4,
     },
+  },
+});
+
+const process_swiper = new Swiper(".process__swiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    }
   },
 });
