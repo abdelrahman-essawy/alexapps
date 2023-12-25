7dd4f445317f93c5db23718d0fe7c5d5207faeb0
Muhammed-Ibrahem <muhammedibrahem001@gmail.com>
2023-12-25 14:37:40 +0200

    add: fresh section (blog posts section)

--- src/js/swipeEffect.js
+++ src/js/swipeEffect.js
@@ -80,3 +80,26 @@ var portfolio = new Swiper(".portf__apps", {
     },
   },
 });
+
+
+const fresh = new Swiper(".fresh__cards", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
