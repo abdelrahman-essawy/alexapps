0a35e0d997a06e619d961ffc7c80e4e4bebc9e83
Muhammed-Ibrahem <muhammedibrahem001@gmail.com>
2023-12-25 10:06:29 +0200

    add: portfolio section to the home-page

--- src/js/swipeEffect.js
+++ src/js/swipeEffect.js
@@ -53,7 +53,32 @@ console.log(testiPagination);
 imgsButtons.forEach((btn) => {
   btn.addEventListener("click", () => {
     console.log(btn.dataset.quoteindex);
-    const clickedImgIndex = btn.dataset.quoteindex - 1
+    const clickedImgIndex = btn.dataset.quoteindex - 1;
     testiPagination[clickedImgIndex].click();
   });
+});
+
+var portfolio = new Swiper(".portf__apps", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+  },
 });
